dca5839992b3b0ee296456cbd41c34d59a629216
Serhii Vasylets <akvatonk@gmail.com>
2025-11-28 16:56:26 +0200

    feat: email confirmation added through ethereal.email,  docker settings prepared

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -50,10 +50,10 @@ describe('Users API', () => {
                 password: 'password',
             });
 
-        expect(res.statusCode).toEqual(201);
-        expect(res.body).toHaveProperty('id');
-        expect(res.body.username).toEqual('User User');
-        expect(res.body.email).toEqual('useruser@gmail.com');
+        expect(res.body.user).toHaveProperty('id');
+        expect(res.body.user.username).toEqual('User User');
+        expect(res.body.user.email).toEqual('useruser@gmail.com');
+        expect(res.body.message).toContain('User created');
 
         expect(res.body).not.toHaveProperty('password');
     });
